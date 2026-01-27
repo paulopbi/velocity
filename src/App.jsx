99925d8d4310b6309fcdb2f6441bf783821d96a0
Paulo Victor <paulovictordev16@gmail.com>
2026-01-26 22:20:08 -0300

    feat(App): add navbar and hero

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "@/layout/Navbar";
+import Hero from "@/sections/Hero/Hero";
+
+function App() {
+	return (
+		<>
+			<Navbar />
+
+			<main>
+				<Hero />
+			</main>
+		</>
+	);
+}
+
+export default App;
