d22263fb1d8c44224a95ec6c5e988a2e3d12e5b0
Paulo Victor <paulovictordev16@gmail.com>
2026-01-29 19:47:35 -0300

    Refactor: adjust card layout and styling, and remove duplicate import.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,26 +1,27 @@
 import Button from "@/components/Button/Button";
 import { cn } from "@/utils/cn";
 import { Check } from "lucide-react";
-import Button from "../Button/Button";
 
 function Card({ info }) {
 	if (info) {
 		return (
 			<section
 				className={cn(
-					"bg-white border border-gray-200 p-8 rounded-xl shadow-lg *:mb-8 min-w-[400px] min-h-[420px]",
-					info.mostPopular && "border-primary bg-black/90 text-white",
+					"overflow-hidden flex flex-col gap-10 items-start justify-center p-5 rounded-xl shadow-lg sm:min-w-[400px] sm:min-h-[400px]",
+					info.mostPopular
+						? "bg-black/90 text-white"
+						: "border-gray-200 bg-white",
 				)}
 			>
 				{/* heading */}
 				<div className="flex flex-col items-start justify-center gap-2">
-					<span className="text-2xl font-semibold leading-10">{info.type}</span>
-					<span className="text-4xl font-semibold leading-10">
+					<span className="text-2xl font-semibold">{info.type}</span>
+					<span className="text-4xl font-bold">
 						{info.price}
 						<span
 							className={cn(
-								"text-base text-muted-foreground ml-2 font-normal",
-								info.mostPopular && "text-white/70",
+								"text-sm ml-1 font-normal",
+								info.mostPopular ? "text-white/70" : "text-muted-foreground",
 							)}
 						>
 							/month
@@ -28,8 +29,8 @@ function Card({ info }) {
 					</span>
 					<p
 						className={cn(
-							"text-muted-foreground leading-5",
-							info.mostPopular && "text-white/70",
+							"text-base font-normal",
+							info.mostPopular ? "text-white/70" : "text-muted-foreground",
 						)}
 					>
 						{info.description}
@@ -37,7 +38,7 @@ function Card({ info }) {
 				</div>
 
 				{/* features */}
-				<ul className="*:mb-2">
+				<ul className="flex flex-col gap-3">
 					{info.features.map((feature) => (
 						<li
 							key={feature.type}
@@ -47,7 +48,7 @@ function Card({ info }) {
 							<span
 								className={cn(
 									"text-base font-normal",
-									info.mostPopular && "text-white",
+									info.mostPopular ? "text-white/70" : "text-black/70",
 								)}
 							>
 								{feature}
