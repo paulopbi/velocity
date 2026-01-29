9dbd4c09fbef5f14eb46ab85a90fa76882e44139
Paulo Victor <paulovictordev16@gmail.com>
2026-01-29 19:53:39 -0300

    refactor: adjust pricing section heading and card container for improved alignment and spacing.

--- src/sections/Pricing/Pricing.jsx
+++ src/sections/Pricing/Pricing.jsx
@@ -6,12 +6,13 @@ function Pricing() {
 		<section id="pricing" className="bg-section/30 py-20">
 			<div className="container mx-auto px-6">
 				{/* heading */}
-				<div className="flex flex-col items-center justify-center gap-1 mb-10">
+				<div className="text-center *:mb-2">
 					<h3 className="title">Simple, transparent pricing</h3>
 					<p className="subtitle">Choose the plan that's right for your team</p>
 				</div>
 
-				<div className="flex flex-col sm:flex-row flex-wrap items-center justify-center gap-6 w-full gap-x-8 gap-y-6">
+				{/* cards */}
+				<div className="flex flex-col sm:flex-row flex-wrap items-stretch justify-center gap-8 w-full gap-x-8 gap-y-6 mt-10">
 					{pricingInfo.map((info) => (
 						<Card key={info.type} info={info} />
 					))}
