0b2a9f636a535755e4e6cc22e9c713540daf4c00
Paulo Victor <paulovictordev16@gmail.com>
2026-01-29 21:32:20 -0300

    refactor: refine Hero section layout and responsiveness

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -6,40 +6,46 @@ import HERO_IMAGE from "/hero-image.jpg";
 function Hero() {
 	return (
 		<section className="mt-35 mb-20">
-			<div className="container mx-auto px-6 grid lg:grid-cols-2 gap-12 items-center">
+			<div className="container-section grid lg:grid-cols-2 gap-10 items-center">
 				{/* left side */}
-				<div className="order-2 sm:order-1 *:mb-6">
-					<Badge className="text-center mx-auto md:mx-0 md:text-left">
-						✨ New: AI-Powered Analytics
-					</Badge>
-					<h1 className="title text-center mx-auto md:text-left">
+				<div className="flex flex-col items-center lg:items-start gap-4 order-2 lg:order-1">
+					{/* top */}
+					<Badge>✨ New: AI-Powered Analytics</Badge>
+					<h1 className="title text-center lg:text-left">
 						Build faster with the modern platform
 					</h1>
-					<p className="subtitle max-w-[45ch] text-center mx-auto md:text-left md:mx-0">
+					<p className="subtitle text-center lg:text-left max-w-[45ch]">
 						Transform your workflow with powerful tools designed for modern
 						teams, get started in minutes and scale to millions.
 					</p>
 
-					<div className="flex items-center justify-center md:justify-start gap-4">
-						<Button
-							variant="primary"
-							size="md"
-							className="flex items-center justify-center gap-2"
-						>
-							Start Free Trial <ArrowRight className="w-5 h-5" />
-						</Button>
-						<Button variant="secondary" size="md">
-							Watch Demo
-						</Button>
-					</div>
+					{/* bottom */}
+					<div>
+						{/* buttons */}
+						<div className="flex items-center justify-center lg:justify-start gap-4 mb-4">
+							<Button
+								variant="primary"
+								size="md"
+								className="flex items-center justify-center gap-2"
+							>
+								Start Free Trial <ArrowRight className="w-5 h-5" />
+							</Button>
+							<Button variant="secondary" size="md">
+								Watch Demo
+							</Button>
+						</div>
 
-					<span className="block text-sm text-muted-foreground text-center mx-auto md:text-left md:mx-0">
-						No credit card required • 14-day free trial
-					</span>
+						{/* label */}
+						<div className="text-center lg:text-left">
+							<span className="text-sm text-muted-foreground">
+								No credit card required • 14-day free trial
+							</span>
+						</div>
+					</div>
 				</div>
 
 				{/* right side */}
-				<div className="shadow-2xl rounded-2xl overflow-hidden order-1 sm:order-2 hover:scale-[1.05] transition-all">
+				<div className="shadow-2xl rounded-2xl overflow-hidden order-1 lg:order-2 hover:scale-[1.05] transition-all lg:max-w-[600px] mx-auto">
 					<img
 						src={HERO_IMAGE}
 						alt="A.I"
