48a162f6609e8829abc3604e72617be3b7014c1c
Paulo Victor <paulovictordev16@gmail.com>
2026-01-30 21:41:13 -0300

    feat(Logo): add classname prop

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,14 @@
+import { cn } from "@/utils/cn";
 import { Zap } from "lucide-react";
 
-function Logo({ href }) {
+function Logo({ href, className }) {
 	return (
 		<a
 			href={href}
-			className="flex items-center justify-center gap-2 hover:gap-3 transition-all duration-300 cursor-pointer text-lg font-semibold md:text-xl"
+			className={cn(
+				"flex items-center justify-center gap-2 hover:gap-3 transition-all duration-300 cursor-pointer text-lg font-semibold md:text-xl",
+				className,
+			)}
 		>
 			<div className="bg-primary p-2 rounded-md">
 				<Zap className="w-5 h-5 text-white" />
